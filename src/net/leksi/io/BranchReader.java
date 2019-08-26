b56be7b5e5dec6dc68685203e772b2034acdd722
Alexey Zakharov <leksi@leksi.net>
2019-08-26 18:42:22 +0300

    first commit

---
+++ src/net/leksi/io/BranchReader.java
@@ -0,0 +1,324 @@
+/*
+ * BranchReader
+ * 
+ * v.0.0.1
+ * 
+ * 23-08-2019
+ *
+ * The MIT License
+ *
+ * Copyright 2019 Alexey Zakharov <leksi@leksi.net>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.leksi.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * The class {@code BranchReader} is for different consumers to 
+ * independently read the same {@code Reader}.
+ *
+ * @author Alexey Zakharov &lt;leksi@leksi.net&gt;
+ * @since JDK1.8
+ */
+abstract public class BranchReader extends Reader {
+    
+    /**
+     * Returns an array of {@code count} new branches {@code BranchReader}
+     * which can be read from the current position of the parent's 
+     * {@code BranchReader}.
+     * 
+     * @param count a number of new branches
+     * @return an array of new branches
+     * @throws IOException if it is closed
+     */
+    abstract public BranchReader[] branch(final int count) throws IOException;
+    
+    /**
+     * Returns a boolean value meaning if the 
+     * {@code BranchReader} is closed.
+     * 
+     * @return   a boolean value meaning if the 
+     *           {@code BranchReader} is closed
+     */
+    abstract public boolean isClosed();
+    
+    /**
+     * A factory method for creation of an {@code BranchReader} object of 
+     * the concrete implementation based on the openned underlying
+     * {@code Reader}. The  reading is possible from the current position of the 
+     * {@code source}.
+     * 
+     * @param source the preliminary openned {@code Reader}
+     * @return root {@code BranchReader} object
+     */
+    static public BranchReader create(final Reader source) {
+        return new Root(source).root();
+    }
+    
+    /**
+     * The class {@code ChunkHolder} is an auxiliary class to support a singly 
+     * linked list of data pieces read from the underlying {@code Reader}. 
+     * <p>
+     * In order to use memory sparingly we point the end chunk which is a point 
+     * of grouth and every branch points its currently read chunk. Thus, chunks 
+     * read by all branches are objects for the GC.
+     */
+    private static class ChunkHolder {
+        /**
+         * The actual chunk length.
+         */
+        private int length = 0;
+        /**
+         * The {@code char} array to contain the data chunk.
+         */
+        private char buffer[] = null;
+        /**
+         * The pointer to the next chunk.
+         */
+        private ChunkHolder next = null;
+        private long offset = 0;
+
+        /**
+         * Creates a chunk of predefined size.
+         * @param chunkSize size of memory in {@code char}s to allocate for the chunk.
+         */
+        private ChunkHolder(final int chunkSize) {
+            buffer = new char[chunkSize];
+        }
+    }
+
+    /**
+     * The class {@code Root} is an infrastructure holder for the <i>tree</i> of 
+     * {@code BranchReader} objects.
+     */
+    private static class Root {
+        /**
+         * The underlying {@code Reader}.
+         */
+        private Reader source = null;
+        /**
+         * The flag indicating that the underlying {@code Reader} is fully read.
+         */
+        private AtomicBoolean isSourceEnded = new AtomicBoolean(false);
+        /**
+         * The list of all branches of the tree.
+         */
+        private final List<Branch> branches = Collections.synchronizedList(
+                new ArrayList<>());
+        /**
+         * The last chunk at the singly linked list of data pieces read from the 
+         * underlying {@code Reader}.
+         */
+        private ChunkHolder endChunk = new ChunkHolder(0);
+        /**
+         * The class {@code Branch} is a concrete implementation of the abstract
+         * {@code BranchReader}.
+         */
+        
+        private class Branch extends BranchReader {
+            /**
+             * The current offset relative to the issue of the 
+             * underlying {@code Reader}.
+             */
+            private long position = 0;
+            /**
+             * The chunk currently being read.
+             */
+            private ChunkHolder chunk = null;
+
+            /**
+             * Creates a branch with a parent if it is given
+             * 
+             * @param parent    a parent branch of the new branch or 
+             *                  {@code null} in the case of root
+             */
+            private Branch(final Branch parent) {
+                if (parent != null) { 
+                    /**
+                     * if the parent is given, just clone it
+                     */
+                    position = parent.position;
+                    chunk = parent.chunk;
+                }
+            }
+
+            @Override
+            public BranchReader[] branch(final int count) throws IOException {
+                BranchReader[] res;
+                
+                if (isClosed()) {
+                    throw new IOException("Cannot branch closed reader");
+                }
+                res = new BranchReader[count];
+                for (int i = 0; i < count; i++) {
+                    res[i] = new Branch(this);
+                    branches.add((Branch) res[i]);
+                }
+                return res;
+            }
+
+            @Override
+            public boolean isClosed() {
+                return !branches.contains(this);
+            }
+
+            @Override
+            public int read(final char[] cbuf, 
+                    final int off, final int len) throws IOException {
+                int res = len;              // returned result
+                int readCount = 0;          // cumulative count of chars copied 
+                                            // from (probably) several chunks
+                
+                if (isClosed()) {
+                    throw new IOException("Cannot read closed reader");
+                }
+
+                if(!isSourceEnded.get()) {
+                    if (position + len > getDataLength()) {
+                        /**
+                         * Should read from the underlying {@code Reader}.
+                         */
+                        readFromSource(position + len);
+                    }
+                }
+                if (isSourceEnded.get()) {
+                    /**
+                     * Correct requested amount if it is less than the 
+                     * rest data amount
+                     */
+                    res = (int)Math.min((long)len, endChunk.offset 
+                            + endChunk.length - position
+                            );
+                }
+                while (readCount < res) {
+                    int from;
+                    int n;
+                    
+                    if (position >= chunk.offset + chunk.length) {
+                        chunk = chunk.next;
+                    }
+                    from = (int)(position - chunk.offset);
+                    n = Math.min(chunk.length - from, res - readCount);
+                    System.arraycopy(chunk.buffer, from, cbuf, off + readCount, n);
+                    position += n;
+                    readCount += n;
+                }
+                return res;
+            }
+
+            @Override
+            public void close() throws IOException {
+                closeBranch(this);
+            }
+        }
+        
+        /**
+         * Returns the length of the data currently read from source.
+         * 
+         * @return the length of the data currently read from source
+         */
+        private synchronized long getDataLength() {
+            return endChunk.offset + endChunk.length;
+        }
+        
+        /**
+         * Reads lacking data from underlying {@code Reader} in case of expected
+         * position plus requested length is greate than actually read data
+         * length
+         * 
+         * @param postionAfterRead position plus requested length
+         * @throws IOException in case of I/O error while reading from the 
+         * underlying {@code Reader}.
+         */
+        private synchronized void readFromSource(final long postionAfterRead) 
+                throws IOException {
+            long dataLength = endChunk.offset + endChunk.length;
+            if (postionAfterRead > dataLength) {
+                /**
+                 * Should read from the underlying {@code Reader}.
+                 */
+                int leftReadCount = (int) (postionAfterRead - dataLength);
+                /**
+                 * Allocate new chunk and add it to list
+                 */
+                endChunk.next = new ChunkHolder(leftReadCount);
+                endChunk.next.offset = dataLength;
+                endChunk = endChunk.next;
+
+                while (leftReadCount > 0) {
+                    int n = source.read(endChunk.buffer,
+                            endChunk.length,
+                            Math.min(leftReadCount,
+                                    endChunk.buffer.length
+                                    - endChunk.length));
+                    if (n <= 0) {
+                        isSourceEnded.set(true);
+                        break;
+                    }
+                    endChunk.length += n;
+                    leftReadCount -= n;
+                }
+            }
+        }
+        
+        /**
+         * Closes branch and the underlying {@code Reader} if all branches are 
+         * closed.
+         * 
+         * @param branch the branch to close
+         * @throws IOException in case of I/O error while closing the underlying 
+         * {@code Reader}.
+         */
+        private synchronized void closeBranch(final Branch branch) throws IOException {
+            branches.remove(branch);
+            if (branches.isEmpty() && source != null) {
+                source.close();
+                source = null;
+            }
+        }
+
+        /**
+         * Creates new {@code Root} object with an underlying {@code Reader}.
+         * 
+         * @param source the underlying {@code Reader}.
+         */
+        private Root(final Reader source) {
+            this.source = source;
+        }
+
+        /**
+         * Creates and returns the root branch
+         * 
+         * @return the root branch
+         */
+        private Branch root() {
+            Branch root = new Branch(null);
+            root.chunk = endChunk;
+            branches.add(root);
+            return root;
+        }
+    }
+}
---
+++ test/net/leksi/io/BranchReaderTest.java
@@ -0,0 +1,241 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Alexey Zakharov <leksi@leksi.net>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package net.leksi.io;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alexey Zakharov <leksi@leksi.net>
+ */
+public class BranchReaderTest {
+    
+    /**
+     * Test of create method, of class BranchReader.
+     * 
+     * We allocate {@code initial_readers_count} Branches and start each in 
+     * a separate thread to read. When half of data read each odd branch give a
+     * branch, each even branch closes. After all we test 
+     * 1) if the total number of 
+     * branches equals to expected;
+     * 2) if the text read by each initial even branch 
+     * concatenated with text read by first branch after initial equals to 
+     * reference text;
+     * 3) if the text read by each initial odd branch 
+     * equals to reference text;
+     * 4) if the texts read by each branch after initials are equal.
+     * Also, to ensure the best code covarage, we test some branch closing 
+     * deals
+     */
+
+    StringBuilder text = new StringBuilder();       // Reference text data
+    String resource = "1.txt";                      // Source text file
+    int initial_readers_count = 123;                // Initial branches number
+    int n_repeats = 100;                            // 
+    Map<Integer, String> strings = Collections.synchronizedMap(new HashMap<>());
+    List<Thread> threads = Collections.synchronizedList(new ArrayList<>());
+
+    @Test
+    public void testCreate() throws Exception {
+        /**
+         * Read source file into refernce text
+         */
+        try(
+            Reader source = new InputStreamReader(getClass().getClassLoader().getResourceAsStream(resource));
+        ) {
+            char buf[] = new char[0x1000];
+            int n = 0;
+            while((n = source.read(buf)) > 0) {
+                text.append(buf, 0, n);
+            }
+        }
+        for(int i = 0; i < n_repeats; i++) {
+            /**
+             * Initialize environment
+             */
+            strings.clear();
+            threads.clear();
+            gen_id.set(0);
+            try(
+                Reader source = new InputStreamReader(getClass().getClassLoader().getResourceAsStream(resource));
+                BranchReader result = BranchReader.create(source);
+            ) {
+                /**
+                 * Collect initial branches
+                 */
+                ArrayList<BranchReader> list = new ArrayList<>();
+                list.add(result);
+                list.addAll(Arrays.stream(result.branch(initial_readers_count - 1)).collect(Collectors.toList()));
+                /**
+                 * Collect initial threads
+                 */
+                Thread[] initial_threads = list.stream().map(br -> new_thread(br)).toArray(Thread[]::new);
+                /**
+                 * Start initial threads
+                 */
+                for(Thread th: initial_threads) {
+                    th.start();
+                }
+                /**
+                 * Wait for all therads finish
+                 */
+                while(threads.size() > 0) {
+                    if(threads.get(0).isAlive()) {
+                        threads.get(0).join();
+                    }
+                    threads.remove(0);
+                }
+                /**
+                 * Test 1)
+                 */
+                assertEquals(initial_readers_count + initial_readers_count / 2, strings.size());
+                for(int k = 0; k < initial_readers_count; k++) {
+                    if(k % 2 == 0) {
+                        /**
+                         * Test 2)
+                         */
+                        assertEquals(text.toString(), strings.get(k) + strings.get(initial_readers_count));
+                    } else {
+                        /**
+                         * Test 3)
+                         */
+                        assertEquals(text.toString(), strings.get(k));
+                    }
+                }
+                /**
+                 * Test 4)
+                 */
+                for(int k = initial_readers_count + 1; k < strings.size(); k++) {
+                    assertEquals(strings.get(initial_readers_count), strings.get(k));
+                }
+            }
+        }
+    }
+    
+    /**
+     * ID generator to separate odd and even threads/branches
+     */
+    AtomicInteger gen_id = new AtomicInteger(0);
+    
+    private Thread new_thread(BranchReader br) {
+        int id = gen_id.getAndIncrement();
+        Thread res = new Thread(() -> {
+            StringBuilder sb = new StringBuilder();
+            /**
+             * temporary buffer of random size
+             */
+            char buf[] = new char[(int)Math.ceil(0x800 * (1 + Math.random()))];
+            int n = 0;
+            /**
+             * Rest length before half for initial branches
+             */
+            int rest_len = id < initial_readers_count ? text.length() / 2 : -1;
+            try {
+                while(true) {
+                    if(rest_len > 0 && rest_len < buf.length) {
+                        n = br.read(buf, 0, rest_len);
+                    } else {
+                        n = br.read(buf);
+                    }
+                    if(n <= 0) {
+                        /**
+                         * Only odd and out of initial branches could come here
+                         */
+                        assertTrue(id % 2 == 1 || id >= initial_readers_count);
+                        strings.put(id, sb.toString());
+                        br.close();
+                        break;
+                    }
+                    sb.append(buf, 0, n);
+                    rest_len -= n;
+                    if(id < initial_readers_count && rest_len == 0) {
+                        /**
+                         * half of the way
+                         */
+                        rest_len = -1;
+                        if (id % 2 == 0) {
+                            /**
+                             * close even
+                             */
+                            br.close();
+                            strings.put(id, sb.toString());
+                            break;
+                        } else {
+                            /**
+                             * branch odd
+                             */
+                            new_thread(br.branch(1)[0]).start();
+                        }
+                    }
+                    /**
+                     * let other threads to play
+                     */
+                    Thread.currentThread().sleep(0, 1);
+                }
+                /**
+                 * test if the branch closed
+                 */
+                assertTrue(br.isClosed());
+                try {
+                    br.read();
+                } catch(Exception ex1) {
+                    /**
+                     * can not read closed branch
+                     */
+                    assertTrue(ex1 instanceof IOException);
+                }
+                try {
+                    br.branch(2314);
+                } catch(Exception ex1) {
+                    /**
+                     * can not branch closed branch
+                     */
+                    assertTrue(ex1 instanceof IOException);
+                }
+            } catch (Exception ex) {
+                fail(ex.getClass().getSimpleName() + ": " + ex.getMessage() + "\n    " + Arrays.stream(ex.getStackTrace()).map(e -> e.toString()).collect(Collectors.joining("\n    ")));
+            }
+        });
+        threads.add(res);
+        return res;
+    }
+    
+    
+}
